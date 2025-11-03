dcfaebff59df9b8317830aac4fcbd81e1e7f6479
galuhmaher <75915814+galuhmaher@users.noreply.github.com>
2025-11-03 19:47:50 +0700

    kedua

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./layouts/**/*.{html,js}",
+    "./content/**/*.{html,md,js}",
+    "./themes/**/*.{html,js}",
+    "./static/**/*.{html,js}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
