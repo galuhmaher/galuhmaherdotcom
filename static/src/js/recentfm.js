92e74cad95aa1d39acef0a3c4258760e5a744360
galuhmaher <75915814+galuhmaher@users.noreply.github.com>
2025-12-14 21:01:04 +0700

    tambah recently played

---
+++ static/src/js/recentfm.js
@@ -0,0 +1,20 @@
+const nowCurrentScript = document.currentScript
+const urlParams = new URLSearchParams(nowCurrentScript.src.split('.js')[1])
+const params = Object.fromEntries(urlParams.entries())
+
+if (params.u)
+{
+    const emoji = params.e ? params.e : '🎧'
+    const nomoji = params.nomoji ? true : false
+    const path = `https://recentfm.rknight.me/now.php?username=${params.u}&emoji=${emoji}&nomoji=${nomoji}`
+    fetch(path)
+    .then((response) => response.json())
+    .then((data) => {
+        console.log(data)
+        nowcontainer = document.createElement('div')
+        nowcontainer.className = 'recent-played'
+        nowcontainer.style
+        nowcontainer.innerHTML = `${data.content}`
+        nowCurrentScript.parentNode.insertBefore(nowcontainer, nowCurrentScript)
+    })
+}
